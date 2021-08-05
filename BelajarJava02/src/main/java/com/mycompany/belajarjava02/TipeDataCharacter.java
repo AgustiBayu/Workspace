d21dd1b4b74d893984daf21dda22bb27731e853a
Agusti Bayu <Agustibayu.ABS@gmail.com>
2021-08-05 21:20:08 +0700

    #2 TipedataCharacter

--- BelajarJava02/src/main/java/com/mycompany/belajarjava02/TipeDataCharacter.java
+++ BelajarJava02/src/main/java/com/mycompany/belajarjava02/TipeDataCharacter.java
@@ -1,21 +1,54 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.mycompany.belajarjava02;
 
-/**
- *
- * @author 62813
- */
+
 public class TipeDataCharacter {
 
-    /**
-     * @param args the command line arguments
-     */
+   
     public static void main(String[] args) {
-        // TODO code application logic here
+        
+        /*
+        untuk menampilkan sebuah nilai yang sudah kita inisialisasi 
+        yaitu dengan menggunakan sebuah Syestem.out.println setelah itu
+        variabelnya perhatikan println dengan print, println digunakan untuk
+        menampilkan nilai setelah itu enter namun jika print saja maka setelah
+        memunculkan data tidak ada enter
+        */
+        
+        char a = 'A';
+        char b = 'B';
+        char c = 'C';
+        
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        
+        /*
+        Tipe data dibawah ini merupakan tipedata boolean yang memiliki
+        2 difault yaitu benar dan salah
+        */
+        
+        boolean benar = true;
+        boolean salah = false;
+        
+        System.out.println(benar);
+        System.out.println(salah);
+        
+        /*
+        Dalam Source code dibawah ini merupakan sebuah tipedata
+        string dan varibel fristname dan lastname yang didalamnya
+        sudah diberi sebuah huruf yang dikurung dengan menggunakan 
+        tanda titik 2
+        */
+        
+        String fristName = "Agusti";
+        String lastName = "Bayu Samudro";
+        String fullname = fristName+ " " +lastName;
+        
+        System.out.println(fristName);
+        System.out.println(lastName);
+        System.out.println(fristName+ " " +lastName);
+        System.out.println(fullname);
+                
     }
     
 }
