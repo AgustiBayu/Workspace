61151a74d1322610bd4b600a454b438ef6c36a7b
DidikH <com.didikhariyanto@gmail.com>
2021-08-24 13:31:37 +0700

    #Update note AppFizzBuzz

--- AppFizzBuzz/src/main/java/programmer/java/now/app/FizzBuzz.java
+++ AppFizzBuzz/src/main/java/programmer/java/now/app/FizzBuzz.java
@@ -8,15 +8,19 @@ public class FizzBuzz {
 
         Scanner s = new Scanner(System.in);
         System.out.print("Input Number ");
-      
+      /* Note
+      - if fizzbuzz 7 dan 9 habis dibagi maka result fizzbuzz
+      - if fizz nilai habis dibagi 7 maka result fizz
+      - if buzz nilai habis dibagi 9 maka result buzz
+      */
         int n = s.nextInt();
         for (int i = 1; i < n; i++) {
-            if ((i % 7 == 0) && (i % 9 == 0)) { // if fizzbuzz 7 dan 9 habis dibagi maka result fizzbuzz
+            if ((i % 7 == 0) && (i % 9 == 0)) {
                 System.out.println("FizzBuzz");
             } else if ((i % 7) == 0) {
-                System.out.println("Fizz"); // if fizz nilai habis dibagi 7 maka result fizz
+                System.out.println("Fizz");
             } else if ((i % 9) == 0) {
-                System.out.println("Buzz"); // if buzz nilai habis dibagi 9 maka result buzz
+                System.out.println("Buzz");
             } else {
                 System.out.println(i);
             }
