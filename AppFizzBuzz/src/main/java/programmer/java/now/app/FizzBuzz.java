0068723aeb32d7a09af6e3d6aacd709ddbe2ea6c
Agusti Bayu <Agustibayu.ABS@gmail.com>
2021-08-24 13:26:32 +0700

    # Update FizzBuzz
    # command result fizzbuzz

--- AppFizzBuzz/src/main/java/programmer/java/now/app/FizzBuzz.java
+++ AppFizzBuzz/src/main/java/programmer/java/now/app/FizzBuzz.java
@@ -8,15 +8,15 @@ public class FizzBuzz {
 
         Scanner s = new Scanner(System.in);
         System.out.print("Input Number ");
-
+      
         int n = s.nextInt();
         for (int i = 1; i < n; i++) {
-            if ((i % 7 == 0) && (i % 9 == 0)) {
+            if ((i % 7 == 0) && (i % 9 == 0)) { // if fizzbuzz 7 dan 9 habis dibagi maka result fizzbuzz
                 System.out.println("FizzBuzz");
             } else if ((i % 7) == 0) {
-                System.out.println("Fizz");
+                System.out.println("Fizz"); // if fizz nilai habis dibagi 7 maka result fizz
             } else if ((i % 9) == 0) {
-                System.out.println("Buzz");
+                System.out.println("Buzz"); // if buzz nilai habis dibagi 9 maka result buzz
             } else {
                 System.out.println(i);
             }
